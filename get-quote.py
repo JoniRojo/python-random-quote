5b229a36775830594b2246f211ee36f4d27d3d02
Jonathan Rojo <86434164+JoniRojo@users.noreply.github.com>
2022-08-29 12:09:34 -0300

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,3 +1,4 @@
+import random
 def primary():
   #print("Keep it logically awesome.")
 
@@ -8,6 +9,10 @@ def primary():
   print(quotes)
   print(quotes[0])
   print(quotes[13])
+  
+  last = 13
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
